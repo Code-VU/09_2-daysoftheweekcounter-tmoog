0d416c6513bb42f519f7d6f5c1c42da5cb61daeb
Taylor Moog <taylormoog@vu.com>
2023-04-03 20:37:12 -0500

    split out lines and used startswith(), then added the 2nd indexed word to the dictionary and added 1 to the value if already in dictionary

--- counter.py
+++ counter.py
@@ -1,6 +1,25 @@
 def countDayOfTheWeek():
     # This first line is provided for you
     file_name = input("Enter a file name: ")
+    fhand = open(file_name)
+    d = dict()
+    for line in fhand:
+        line = line.rstrip()
+        words = line.split()
+        if line.startswith('From'):
+            if line.startswith('From:'):
+                continue
+            else:
+                day = words[2]
+                if day not in d:
+                    d[day] = 1
+                else:
+                    d[day] += 1
+    print(d)
+
+
+
+
 
 
 
